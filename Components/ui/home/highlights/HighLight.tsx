0b4e77df6509026a4f24c7be63afe24e35df406a
unknown <chijiokerichard61@gmail.com>
2026-02-09 15:08:25 +0100

    corrected my higlight props

--- Components/ui/home/highlights/HighLight.tsx
+++ Components/ui/home/highlights/HighLight.tsx
@@ -8,8 +8,9 @@ type highlightProps = {
     amount:string;
     percentage:string;
     title:string;
+    color?:string;
 }
-export default function HighLight({img,amount,percentage,title}:highlightProps) {
+export default function HighLight({img,amount,percentage,title,color}:highlightProps) {
   return (
     <div className=" w-71 bg-accent h-full p-3 rounded-md flex items-center justify-between">
         <div className="h-18` w-23 gap-2.5 flex flex-col">
@@ -17,7 +18,7 @@ export default function HighLight({img,amount,percentage,title}:highlightProps) 
            <div className="w-23 h-10 gap-0.5 flex flex-col">
 
             <h4 className={`${plusjarta.className} text-[20px] text-[#191919] font-bold leading-[150%] tracking-[-3%] `}>${amount}</h4>
-            <span className={`${plusjarta.className} text-[14px] text-[#33B843] font-semibold leading-[150%] tracking-[-2%] `}>+{percentage}%</span>
+            <span style={{color:color}} className={`${plusjarta.className} text-[14px] text-[#33B843] font-semibold leading-[150%] tracking-[-2%] `}>{percentage}%</span>
            </div>
         </div>
       {img}
--- Components/ui/home/highlights/HighlightsComp.tsx
+++ Components/ui/home/highlights/HighlightsComp.tsx
@@ -7,7 +7,7 @@ function HighlightsComp() {
     <div className="flex  items-center justify-between w-full gap-1">
     <HighLight img={<BarChartIcon color="#000" size={60}  />} title='New client' amount='20' percentage='20'/>
     <HighLight img={<BarChartIcon color="#000" size={60}  />} title='New client' amount='20' percentage='20'/>
-    <HighLight img={<BarChartIcon color="#000" size={60}  />} title='New client' amount='20' percentage='20'/>
+    <HighLight img={<BarChartIcon color="#000" size={60}  />} title='New client' color="red" amount='20' percentage='-5'/>
     </div>
   )
 }
